9afeee309ed4500cb17ba0f6afcbac965ba6bd65
Glen <glencbz@gmail.com>
2015-11-27 01:03:15 +0800

    fixed bugs

--- edimensionDownloader.user.js
+++ edimensionDownloader.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         eDimension Download Link
 // @namespace    https://github.com/glencbz/edimensionDownloadLink
-// @version      0.93
+// @version      0.94
 // @description  Adds a download link for eDimension
 // @author       Glen Choo
 // @match        http://edimension.sutd.edu.sg/course/view.php?id=*
@@ -25,8 +25,29 @@ var requestHeaders = {
 	"Accept-Language": "en-GB,en-US;q=0.8,en;q=0.6",
 	"Cookie": document.cookie,
 };
-var anchors = $(".mod-indent>a");
-function requestAppend(anchor, i){
+
+$("head").append('<style type="text/css"> .download-link {margin-left:2%;}</style>');
+$("head").append('<style type="text/css"> .progress-bar-back {position: fixed; width: 13%; top: 0; right: 0; background-color: #E0E0E0;}</style>');
+$("head").append('<style type="text/css"> .progress-bar-front {position: relative; width: 0%; background-color: #4AC74A;}</style>');
+$("head").append('<style type="text/css"> .progress-bar {height: 25px; border-radius: 3px; transition: ease-in 0.1s;}</style>');
+
+$("body").append('<div class="progress-bar-back progress-bar"></div>');
+$(".progress-bar-back").append('<div class="progress-bar-front progress-bar"></div>');
+
+var originalAnchors = $(".mod-indent>a");
+var anchors = [];
+var progress = 0;
+var requestNums = 0;
+for (var i = 0; i < originalAnchors.length; i++){
+	var $anchor = $(originalAnchors[i]);
+	if ($anchor.children("a.download-link").length < 1 && $anchor.children("img").attr("src") == "http://edimension.sutd.edu.sg/theme/image.php/campus/core/1434085985/f/pdf")
+		anchors.push($anchor);
+}
+
+requestNums = anchors.length;
+
+function requestAppend(i){
+	var anchor = anchors[i];
 	GM_xmlhttpRequest({
 		method: "get",
 		headers: requestHeaders,
@@ -34,28 +55,27 @@ function requestAppend(anchor, i){
 		onreadystatechange: function(response){
 	        if (response.readyState != 4)
 	            return;
-	        if (--i >= 0)
-		        anchorCheck(i);
+	        if (i > 0)
+		        requestAppend(i - 1);
+		    console.log(i);
 		    var responseDom = $.parseHTML(response.responseText);
 	        var $responseDom = $(responseDom);
 	        var objectNode = $responseDom.find("object");
 	        var resourceUrl;
-	        if (objectNode.length > 1) 
-	        	resourceUrl = objectNode.attr("href")
-	        
+	        if (objectNode.length !== 0) 
+	        	resourceUrl = objectNode.attr("data")
 	        else
-	        	resourceUrl = $(".resourceworkaround>a").attr("href");
-	        
+	        	resourceUrl = $responseDom.find(".resourceworkaround>a").attr("href");
         	var style = "color:green";
         	var innerHTML = "Download";
 			var matchingAnchors = $("a[href='" + anchor.attr('href') + "']");
-			matchingAnchors.each(function(i, anchor){
+			matchingAnchors.each(function(index, anchor){
 					var $anchor = $(anchor);
-					if ($anchor.find(".download-link").length === 0)
+					if ($anchor.find(".download-link").length === 0){
 						$anchor.append('<a href=' + resourceUrl + ' download target="_blank" style='+ style + ' class="download-link download' + i + '">' + innerHTML+'</a>');
+					}
 				}
 			);
-
 			progress += 1;
 			$(".progress-bar-front").css("width", (progress/requestNums) * 100 + "%" );
 			if (progress === requestNums){
@@ -65,38 +85,7 @@ function requestAppend(anchor, i){
 		},
 	});	
 }
-$("head").append('<style type="text/css"> .download-link {margin-left:2%;}</style>');
-$("head").append('<style type="text/css"> .progress-bar-back {position: fixed; width: 13%; top: 0; right: 0; background-color: #E0E0E0;}</style>');
-$("head").append('<style type="text/css"> .progress-bar-front {position: relative; width: 0%; background-color: #4AC74A;}</style>');
-$("head").append('<style type="text/css"> .progress-bar {height: 25px; border-radius: 3px; transition: ease-in 0.1s;}</style>');
-
-$("body").append('<div class="progress-bar-back progress-bar"></div>');
-$(".progress-bar-back").append('<div class="progress-bar-front progress-bar"></div>');
-
-var progress = 0;
-var requestNums = 0;
-for (var i = anchors.length - 1; i >= 0; i--){
-	if ($(anchors[i]).children("img").attr("src") == "http://edimension.sutd.edu.sg/theme/image.php/campus/core/1434085985/f/pdf")
-		requestNums += 1;
-}
-
-var i = anchors.length;
-function anchorCheck(i){
-	try{
-		var $anchor = $(anchors[i]);
-		if ($anchor.children("img").attr("src") == "http://edimension.sutd.edu.sg/theme/image.php/campus/core/1434085985/f/pdf"){
-			requestAppend($anchor, i);
-		}else{
-			if(--i >= 0)
-				anchorCheck(i);
-		}
-	}
-	catch (err){
-		console.log(err);
-	}
-}
-
-anchorCheck(i);
+requestAppend(anchors.length - 1);
 
 $(".mod-indent>a").prop("onclick", null);
 });
